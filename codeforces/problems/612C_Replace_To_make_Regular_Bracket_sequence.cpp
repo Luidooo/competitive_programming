71867397c354b12768794116d32872ac14bfc8b8
Luis Eduardo Lima <eng.limaluis@gmail.com>
2025-09-23 07:57:06 -0300

    ac: codeforces-612C

--- codeforces/problems/612C_Replace_To_make_Regular_Bracket_sequence.cpp
+++ codeforces/problems/612C_Replace_To_make_Regular_Bracket_sequence.cpp
@@ -1,30 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
- * pode substituir um aberto por um fechados, mas o contrario nao
- * o algoritmo já indetinfica se a quant de aberto e fechado é a mesma
- * mas nao verifica quantos precisamos mudar, podemos usar 3 pilhas diferentes para resolver isso
-*/
- 
-int main(){
+int compops(char k, char kop) {
+  if (k == '<' && kop == '>')
+    return 0;
+  if (k == '(' && kop == ')')
+    return 0;
+  if (k == '{' && kop == '}')
+    return 0;
+  if (k == '[' && kop == ']')
+    return 0;
+  return 1;
+}
+
+int main() {
   stack<char> q;
-  for(char k; cin >> k;){
-    if(k == '<' || k == '(' || k == '{' || k == '[')
+  int sum = 0;
+  for (char k; cin >> k;) {
+    if (k == '<' || k == '(' || k == '{' || k == '[') {
       q.push(k);
-    else{
-      if(!(q.empty())) {
+    } else {
+      if (!(q.empty())) {
+        char w = q.top();
+        sum += compops(w, k);
         q.pop();
-        continue;
-      }
-      else{
-        cout << "impossible" << endl;
+      } else {
+        cout << "Impossible" << endl;
         return 0;
       }
     }
-
   }
 
-  cout << q.size() << endl;
-   
+  if (!q.empty()) {
+    cout << "Impossible" << endl;
+  } else {
+    cout << sum << endl;
+  }
 }
